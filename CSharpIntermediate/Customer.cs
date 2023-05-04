bae8f743f501d081dc4236e7d9bdd5683277168a
Audrea Cook <audrea.cook@dutchie.com>
2023-05-04 16:06:49 -0400

    Add comments

--- CSharpIntermediate/Customer.cs
+++ CSharpIntermediate/Customer.cs
@@ -8,19 +8,19 @@ namespace CSharpIntermediate
         public string Name;
         public List<Order> Orders;
 
-        public Customer()
+        public Customer() // Constructor
         {
-            Orders = new List<Order>();
+            Orders = new List<Order>(); // Initialize an empty list to avoid null reference exceptions
         }
 
-        public Customer(int id)
-            : this()
+        public Customer(int id) // Constructor overloading
+            : this() // Constructor chaining includes the default constructor 
         {
             this.Id = id;
         }
 
-        public Customer(int id, string name)
-            : this(id)
+        public Customer(int id, string name) // Constructor overloading with chaining
+            : this(id) // Constructor chaining includes the constructor with one parameter
         {
             this.Name = name;
         }
